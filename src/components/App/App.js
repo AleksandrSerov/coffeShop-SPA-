a3387d15ad60701b5371b5108f8188791ddd6933
Aleksandr Serov <sashaserov2010@gmail.com>
2019-02-09 16:52:45 +0300

    Add component CoffePage

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
 import './app.sass';
 import Main from '../Main/Main';
+import CoffePage from '../CoffePage/CoffePage';
+
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-			<Main/>
+			{/* <Main/> */}
+			<CoffePage/>
 			</div>
 		);
 	}
